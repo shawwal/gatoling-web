7143af3e1db6f8d3631a32baef78a99ce52e0dd9
Muhammad Syawaludin <syawalhastaf@gmail.com>
2023-10-21 13:39:23 +0800

    add basic landing page for gatoling app

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Gatoling</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/shawwal/gatoling-web',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/ReGJ9Dgp',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/shawwal/gatoling-web',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '©2023 Gatoling - All rights reserved.',
   },
 }
 

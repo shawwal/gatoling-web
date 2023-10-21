8b6f43028255310f4259e0b30ec325493a24af62
Muhammad Syawaludin <syawalhastaf@gmail.com>
2023-10-21 13:51:57 +0800

    adjust SEO, Meta and Minor Details

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,18 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Gatoling</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Gatoling" />
+      <meta property="og:description" content="Master Russian, English, Malay, and Indonesian with Gatoling - Your Gateway to Language Learning. Discover an engaging and effective way to learn languages, from alphabets to 850 common words. Join our global language learning community today and embark on a journey of cultural enrichment and career opportunities." />
+    </>
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Gatoling'
+    }
+  },
   project: {
     link: 'https://github.com/shawwal/gatoling-web',
   },

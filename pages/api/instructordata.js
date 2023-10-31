9ac913ac811088d33c8c79e3fbc46dbada60d703
Muhammad Syawaludin <syawalhastaf@gmail.com>
2023-10-31 08:05:45 +0800

    add language instructor list

---
+++ pages/api/instructordata.js
@@ -0,0 +1,18 @@
+const instructorData = [
+  {
+    instructorName: "Yana Zukovskaya",
+    coachTitle: "Top Coach",
+    verified: true,
+    accent: "American",
+    nationality: "Russian",
+    language: "🇷🇺 🇺🇸",
+    bookUrl: "https://www.instagram.com/russiannative_/",
+    avatarUrl: "https://gatoling.shawwals.com/instructors/yana.png",
+    personalDescription:
+      "Hello, if you're learning Russian, then you're in the right place ;)",
+  },
+];
+
+export default function handler(req, res) {
+  res.status(200).json({ instructorData });
+}
